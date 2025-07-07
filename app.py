c2796e5354ab2050517e0226c17ae6b32d643c16
Subhash pandit <subhashpandit511@gmail.com>
2025-07-07 14:28:34 +0530

    udate done

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ CORS(app)
 def receive_stock():
     data = request.json
     print("Received stock data:", data)
-    url = 'https://script.google.com/macros/s/AKfycbxVRTjf3Lyl7EM0wEN2HdSyRSRa6QejaEPKH_19L9pAzzQCj440HRh78SSCF0zZda671w/exec'
-    response = requests.post(url, json=data)
+    # url = 'https://script.google.com/macros/s/AKfycbxVRTjf3Lyl7EM0wEN2HdSyRSRa6QejaEPKH_19L9pAzzQCj440HRh78SSCF0zZda671w/exec'
+    # response = requests.post(url, json=data)
 
-    # Print response
-    print("Google Status Code:", response.status_code)
-    print("Google Response Text:", response.text)
+    # # Print response
+    # print("Google Status Code:", response.status_code)
+    # print("Google Response Text:", response.text)
     # Save data in json file
     with open("response.txt", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
